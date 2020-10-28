91245d86461e5d9bca0f3bd6ea1a2319ddf343a6
RDOToole89 <roibinotoole@gmail.com>
2020-10-28 23:13:16 +0100

    first commit, setup express

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.port || 4000;
+
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
+// parse application/json
+app.use(express.json());
+
+app.get("/", (req, res, next) => {
+  res.json("Hello World!");
+});
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
